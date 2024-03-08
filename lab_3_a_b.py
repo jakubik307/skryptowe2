79f180cea4094dcaf5edcb22df22f74ec3221127
Jakub Czajkowski <272709@student.pwr.edu.pl>
2024-03-08 15:25:53 +0100

    Add zad 3e and fix zad 3a

--- lab_3_a_b.py
+++ lab_3_a_b.py
@@ -1,12 +1,8 @@
-import sys
-
 import lab_3_helpers
 
 
-def print_status_302():
-    for line in sys.stdin:
-        if lab_3_helpers.check_status_in_line(line, 302):
-            print(line)
+def print_status_302_quantity():
+    print(f"Status 302 request count: {lab_3_helpers.count_codes(302)}")
 
 
-print_status_302()
+print_status_302_quantity()
