bb3975a8ecb8d6125e5cfdbb2f9ade97a2d1d707
Jakub Czajkowski <272709@student.pwr.edu.pl>
2024-03-08 14:59:00 +0100

    zad 3d

---
+++ lab_3_d.py
@@ -0,0 +1,29 @@
+import sys
+
+import lab_3_helpers
+
+
+def print_image_download_ratio():
+    image_data = 0
+    total_data = 0
+
+    for line in sys.stdin:
+        if lab_3_helpers.get_request_from_line(line) == "GET":
+            bytes_in_line = lab_3_helpers.get_bytes_from_line(line)
+            total_data += bytes_in_line
+            if is_image(lab_3_helpers.get_path_from_line(line)):
+                image_data += bytes_in_line
+
+    print(f"Image download ratio: {image_data / total_data}")
+
+
+def is_image(path):
+    image_extensions = ('.gif', '.jpg', '.jpeg', '.xbm')
+
+    for extension in image_extensions:
+        if extension in path:
+            return True
+    return False
+
+
+print_image_download_ratio()
