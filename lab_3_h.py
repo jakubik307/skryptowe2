a9db75fd5c03c21f87ff568c55ffc74ddd16d968
Apolonia Abramowicz <pola.abramowicz@onet.pl>
2024-03-09 19:21:16 +0100

    3 gotowe

--- lab_3_h.py
+++ lab_3_h.py
@@ -2,10 +2,10 @@ import sys
 import lab_3_helpers
 
 
-def print_sent_from_poland():
+def print_requests_sent_from_poland():
     for line in sys.stdin:
         if lab_3_helpers.check_country(line, "pl"):
             print(line)
 
 
-print_sent_from_poland()
+print_requests_sent_from_poland()
