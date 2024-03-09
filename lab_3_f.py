33dc28ef9e63b04f2134da36c4d8514bac14fa8e
Apolonia Abramowicz <pola.abramowicz@onet.pl>
2024-03-09 19:10:57 +0100

    3f)g)

---
+++ lab_3_f.py
@@ -0,0 +1,11 @@
+import sys
+import lab_3_helpers
+
+
+def print_filter_sent_between_22_and_6():
+    for line in sys.stdin:
+        if lab_3_helpers.check_hours_range(line, 22, 6):
+            print(line)
+
+
+print_filter_sent_between_22_and_6()
--- lab_3_helpers.py
+++ lab_3_helpers.py
@@ -54,3 +54,36 @@ def check_week_day(line, day):
 def get_week_day(line):
     date = datetime.datetime.strptime(line[1:], "%d/%b/%Y:%H:%M:%S")
     return int(date.weekday())
+
+
+def check_hours_range(line, start_hour, end_hour):
+    try:
+        return check_given_hour(line.split(" ")[3].strip(), start_hour, end_hour)
+    except ValueError:
+        return False
+    except IndexError:
+        return False
+
+
+def check_given_hour(line, start_hour, end_hour):
+    hour = line.split(":")
+    h = int(hour[1].strip())
+    m = int(hour[2].strip())
+    s = int(hour[3].strip())
+    if start_hour < end_hour:
+        if start_hour < h < end_hour:
+            return True
+        if start_hour == h:
+            if m > 0 or s > 0:
+                return True
+        return False
+    else:
+        if h < end_hour:
+            return True
+        if start_hour < h:
+            return True
+        if start_hour == h:
+            if m > 0 or s > 0:
+                return True
+        return False
+
